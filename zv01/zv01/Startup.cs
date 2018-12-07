9a418f7f1208e1e4f77e0ec7b2dc58e3f8c01b0f
Peio Murguia <peiomurguia@gmail.com>
2018-12-07 15:10:50 +0100

    Nada nuevo

--- zv01/zv01/Startup.cs
+++ zv01/zv01/Startup.cs
@@ -34,8 +34,7 @@ namespace zv01
                 // This lambda determines whether user consent for non-essential cookies is needed for a given request.
                 options.CheckConsentNeeded = context => true;
                 options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-          
+            });          
 
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(
